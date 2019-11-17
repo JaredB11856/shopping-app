b60146f8f5ffb2e24d3b7a7e13c90b500f32e8fd
Jared Brown <Jb_90210@hotmail.com>
2019-11-17 15:27:09 -0700

    Updated views & models, fixedbug in routes, created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!(    
+    name: "Shoe 1",
+    description: "Duis pharetra gravida ante in porttitor. Nunc imperdiet efficitur diam vitae maximus. Proin vel suscipit dui. Quisque a nibh ac mi luctus luctus non id sem. Proin quis nulla eget odio commodo consectetur rutrum ut urna. Fusce vitae venenatis diam, quis ornare nibh. Aenean ornare eros vel sapien efficitur, a consequat diam sagittis. Morbi lorem nulla, gravida vitae consectetur vitae, blandit a mi. Suspendisse fermentum egestas nisi mattis vulputate. Donec vel consectetur magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus vulputate et neque ac pretium. Phasellus ullamcorper imperdiet lectus, ut tempor dui feugiat vel. Suspendisse potenti. Aliquam in eros nulla.",
+    price: "129.99",   
+    
+  )    
+
+Category.create!(    
+    name: "Shoes"    
+  )    
